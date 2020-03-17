56f4e16a20b1e76090fcd4fd844473c8ed53fb75
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-03-16 22:41:34 -0400

    added html in public folder and added get and post

--- orm.js
+++ orm.js
@@ -37,12 +37,36 @@ const db = new Database({
     port: 3306,
     user: "root",
     password: "akanksha12",
-    database: "eatDaBurger" 
+    database: "burgerlog" 
 });
 
-async function saveUserBurger(userBurger){
-    const saveBurgerDb = await db.query("INSERT INTO burgerLog(name) VALUES(?)",[userBurger.name]);
-    console.log(saveBurgerDb[0]);
+async function saveBurgerName(userBurger){
+    const burgerSqlSave = await db.query("INSERT INTO burgerNames(name) VALUES(?)", [userBurger.name]);
+    console.log(burgerSqlSave[0]);
+}
+
+async function getBurgerName(){
+    const getBurgerNameSql = await db.query("SELECT * FROM burgerNames wHERE devoured=0");
+    console.log(getBurgerNameSql);
+    return getBurgerNameSql;
+}
+
+async function devouredBurger(burgerId){
+    const updateBurgerState = await db.query("UPDATE burgerNames SET devoured=? WHERE id=?", [true, burgerId]);
+    console.log("Burger stated updated")
+    console.log(updateBurgerState);
+}
+
+async function getdevourededBurger(){
+    const getBurger = await db.query("SELECT * FROM burgerNames wHERE devoured=1");
+    console.log("Burger devoured gotten");
+    return getBurger;
+}
+module.exports = {
+    saveBurgerName,
+    getBurgerName,
+    devouredBurger,
+    getdevourededBurger
 
 }
 
--- server.js
+++ server.js
@@ -7,10 +7,34 @@ const app  = express();
 app.use( express.static('public'));
 app.use( express.urlencoded({ extended: false }));
 
-app.post("/api/choiceOfBurger", async function(req,res){
-    const Savedburger = await orm.saveUserBurger(req.body);
-    console.log(Savedburger);
-    res.send("Saved !!")
+
+app.post("/api/burgerChoice", async function(req, res){
+    const savedBurger = await orm.saveBurgerName(req.body);
+    console.log(savedBurger);
+
+    res.send("Success!!")
+
+});
+
+
+app.get("/api/burgerChoice", async function(req, res){
+    const getBurger = await orm.getBurgerName();
+    res.send(getBurger);
+
+});
+
+app.put("/api/burgerUpdate/:id", async function(req, res){
+    console.log(req.params.id)
+    const updateBurger = await orm.devouredBurger(req.params.id);
+
+    res.send("Success!!")
+
+});
+
+//getting the updated burgerInfo
+app.get("/api/updatedBurger", async function(req, res){
+    const getUpdated = await orm.getdevourededBurger();
+    res.send(getUpdated)
 });
 
 app.listen( PORT, function(){
